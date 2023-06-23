136312bed4c158b1c52e62ded55148125cd24402
Nathanuel Dixon <nathan0322@hotmail.com>
2023-06-22 23:19:53 -0400

    deploying

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getFirestore, setDoc, doc, getDoc, collection, addDoc } from "firebase/firestore";
 import {ref, getStorage, uploadBytes } from "firebase/storage";
-import { configobj } from './config.js';
 
 const firebaseConfig = {
-    apiKey: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_KEY : configobj.REACT_APP_API_KEY, 
-    authDomain: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_AUTH_DOMAIN : configobj.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROJECT_ID : configobj.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_STORAGE_BUCKET : configobj.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_MESSAGING_SENDER_ID : configobj.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_APP_ID : configobj.REACT_APP_APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY, 
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
